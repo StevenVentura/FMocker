2def7981c26b881b42b5f236eb534a9b67a6bede
Steven Ventura <8stevenventura@gmail.com>
2018-07-22 11:07:49 -0500

    initial commit

---
+++ FMocker/FClip.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMocker
+{
+    class FClip
+    {
+        //record,save,listen,play
+
+        //Record() puts the last 5 seconds of audio into an FClip object
+        public void AddLastXSecondsToList(long x)
+        {
+
+
+
+        }
+
+        //only run once; thread to actually keep the last5 seconds of sound in memory
+        public void StartRecording()
+        {
+
+
+        }
+
+        //plays the selected sound back through the speaker
+        public void Listen(FClip clip)
+        {
+
+
+        }
+
+        //plays sound thru mic
+        public void PlayThroughMic(FClip clip)
+        {
+            //idk how yet
+        }
+
+
+        public void SaveToFile(FClip clip)
+        {
+
+
+
+        }
+
+
+    }
+}
